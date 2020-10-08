3d520765aae9b5ef79cdf50dad6271b223cf533e
Timotheediot <timotheediot@gmail.com>
2020-10-08 20:23:04 +0200

    responsive content

--- front/src/Components/form/form.js
+++ front/src/Components/form/form.js
@@ -3,16 +3,16 @@ import Porsche from "../../assets/img/porscheModel.png";
 
 const Form = () => {
   return (
-    <div className="bg-gray-800 w-full w-full flex">
-      <div className="w-1/2">
+    <div className="bg-gray-800 w-full flex flex-wrap md:flex-no-wrap">
+      <div className="w-full md:w-1/2 lg:w-1/2">
         <img src={Porsche} alt="porsche4s" />
         <h2 className="italic text-orange-700 text-3xl font-light text-center">
           "Trouvez votre nouvelle voiture électrique en toute simplicité"
         </h2>
       </div>
-      <div className="w-1/2 bg-green-800  rounded-md p-10 m-10">
+      <div className="w-full md:w-1/2 bg-green-800 rounded-md p-10 m-10">
         <input
-          className="w-full text-gray-600 border-2 border-orange-400 bg-gray-800 h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none mt-10 mb-10"
+          className="w-full text-gray-600 border-2 border-orange-400 bg-gray-800 h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none  mb-10"
           type="search"
           name="search"
           placeholder="Marque"
