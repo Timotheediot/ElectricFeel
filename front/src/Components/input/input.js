b197661de3cf3b64a7914bfbd336758a70241007
Timotheediot <timotheediot@gmail.com>
2020-10-25 09:58:13 +0100

    axios to test on a select input

--- front/src/Components/input/input.js
+++ front/src/Components/input/input.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 const Input = () => {
+  const [marque, setMarque] = useState("Marque");
   return (
     <div>
       <input
         className="w-full text-gray-600 bg-gray-800 h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none mb-10"
         type="select"
         name="search"
-        placeholder="Marque"
+        placeholder={marque}
       />
     </div>
   );
